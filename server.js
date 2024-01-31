60d8e34b11f5ca94edd16f7e024c146879c812f3
Aidan <aidanparkhurst@gmail.com>
2024-01-31 14:38:40 -0500

    start

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, '.')));
+
+const PORT = 9999;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
